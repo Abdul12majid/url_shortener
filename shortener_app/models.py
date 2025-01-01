90e00881846c7aa1f488d4ee16b91f707615b04e
Abdul12majid <yisaabdulmajid@gmail.com>
2025-01-01 15:09:27 +0100

    created model

--- shortener_app/models.py
+++ shortener_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class URL(models.Model):
+    original_url = models.URLField()
+    short_code = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
