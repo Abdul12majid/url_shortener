a60f6c2d6a1a0054816288e0b1b30a370f0596a7
Abdul12majid <yisaabdulmajid@gmail.com>
2025-01-02 13:31:08 +0100

    redirect to url view2

--- shortener_app/views.py
+++ shortener_app/views.py
@@ -17,3 +17,12 @@ def create_short_url(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def resolve_short_url(request, short_code):
+    #Redirect to the original URL using the short code.
+    
+    url_instance = get_object_or_404(URL, short_code=short_code)
+    return redirect(url_instance.original_url)
+    
