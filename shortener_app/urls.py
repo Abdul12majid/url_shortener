893fcf4b3faae5dcc5d03acea653c565fa86ec5f
Abdul12majid <yisaabdulmajid@gmail.com>
2025-01-02 13:30:52 +0100

    redirect to url view

--- shortener_app/urls.py
+++ shortener_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('urls/', views.create_short_url, name='create_short_url'),
+    path('urls/<str:short_code>/', views.resolve_short_url, name='resolve_short_url'),
     
 ]
